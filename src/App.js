3715ead642cd15d1a03efe9c12e75f3d61de5ffb
Vikram Babu Rajendran <vk712598@dal.ca>
2022-01-23 22:46:15 -0400

    Edited content in App - first push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I just edited this content !
         </p>
         <a
           className="App-link"
