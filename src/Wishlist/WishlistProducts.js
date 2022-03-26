d64505929e8a0d0cfe5c871c971673019cb77e88
Shivam Miteshkumar Barot <sh895418@dal.ca>
2022-03-26 15:51:00 -0300

    applied css in retrieving wishlist items

--- src/Wishlist/WishlistProducts.js
+++ src/Wishlist/WishlistProducts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './WishlistProducts.css';
 import Grid from '@material-ui/core/Grid';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box } from '@material-ui/core';
 import { Checkbox } from "@material-ui/core";
 import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 import Favorite from "@material-ui/icons/Favorite";
@@ -12,59 +12,76 @@ import { useState, useEffect } from 'react';
 export default function WishlistProducts() {
 
     const [products, setProducts] = useState([]);
-    const [adDetails, setAdDetails] = useState([]);
+    //const [adDetails, setAdDetails] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:8080/wishlist/user")
             .then((response) => {
-                console.log(products[0]["ad_details"]);
-                setAdDetails(products[0]['ad_details']);
+                console.log(response.data.data);
+                setProducts(response.data.data);
+                //console.log(products[0]["ad_details"]);
+                //console.log(products);
+                //setAdDetails(products[0]);
+                //console.log(adDetails);
             })
             .catch((error) => {
                 console.log(error);
             });
     }, []);
 
+    // const handleSubmit = (e) => {
+
+    //     e.preventDefault();
+
+    //     axios.post(api_url, {
+    //         email: formValues.email,
+    //         password: formValues.password
+    //     }).then((response) => {
+
+    //         navigate("/Profile");
+    //     });
+    // };
+
     return (
         <>
-            <h1> &nbsp; &nbsp; Wishlisted Products</h1>
+            <Box sx={{ p: 6 }}>
+                <h1> &nbsp; &nbsp; Wishlisted Products</h1>
+                <Grid container spacing={2} align="justify" >
+                    {products.map((product, index) => {
 
-            <Grid container spacing={2} align="justify" >
+                        return (
+                            <Grid item xs={12} md={6} lg={3}>
+                                <Card elevation={12}>
+                                    <CardMedia
+                                        component="img"
+                                        height="150"
+                                        image={product["ad_details"].image_url}
+                                        alt="Product"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {product["ad_details"].title}
+                                        </Typography>
+                                        <Typography gutterBottom component="div">
+                                            {product["ad_details"].description}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" style={{
+                                            borderRadius: 10,
+                                            backgroundColor: "#21b6ae",
+                                            padding: "9px 18px",
+                                            fontSize: "12px"
+                                        }}>Add to Cart</Button>
 
-                {products.map((product, index) => {
-
-                    return (
-                        <Grid item xs={12} md={6} lg={3}>
-                            <Card elevation={12}>
-                                <CardMedia
-                                    component="img"
-                                    height="150"
-                                    image={adDetails.image_url}
-                                    alt="Product"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {adDetails.title}
-                                    </Typography>
-                                    <Typography gutterBottom component="div">
-                                        {adDetails.description}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button size="small" style={{
-                                        borderRadius: 10,
-                                        backgroundColor: "#21b6ae",
-                                        padding: "9px 18px",
-                                        fontSize: "12px"
-                                    }}>Add to Cart</Button>
-
-                                    <label>Remove from Wishlist<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} defaultChecked /></label>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    )
-                })}
-            </Grid>
+                                        <label>Remove from Wishlist<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} defaultChecked /></label>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Box>
         </>
     )
 }
