691bd3967df1a11442649ab92a8eecd794aa2c63
Vikram Babu Rajendran <vk712598@dal.ca>
2022-03-29 14:27:19 -0300

    Import changes

--- src/Authentication/js/Logout.js
+++ src/Authentication/js/Logout.js
@@ -1,8 +1,8 @@
 //Author: Sowjanya Mani
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react/cjs/react.development";
+
 
 export default function Logout(props) {
 
